4766e9807707eb9fd708692500e891a9e7bd4f5d
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2024-11-01 15:51:04 -0400

    Reports fixed

--- backend/app/routes/summary_routes.py
+++ backend/app/routes/summary_routes.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, Form, HTTPException, Body
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from fastapi.responses import FileResponse
 
 from fastapi import FastAPI, HTTPException, Request, status
 
@@ -21,6 +22,7 @@ import logging
 from fastapi.staticfiles import StaticFiles
 
 from app.services.rag_service import summarize_document, query_chat
+from app.services.report_service import ReportService
 from app.services.tools import tools
 
 # Initialize the router for document routes
@@ -60,31 +62,17 @@ async def summarize_endpoint(
         logging.error(f"An error occurred during the summary process: {str(e)}")
         raise HTTPException(status_code=500, detail=f"An error occurred while summarizing the document: {str(e)}")
 
-@router.post("/report", tags=["Summary"])
-async def summarize_endpoint(
-        publication_id: int,
-        summary_request: Dict[str, str] = Body(...),
-        token: str = Depends(oauth2_scheme),
-        db: Session = Depends(get_db)
-):
-    """
-    Endpoint to summarize a document.
-    """
+class ReportRequest(BaseModel):
+    pdf_name: str
+@router.post("/generate_report", response_class=FileResponse, status_code=status.HTTP_200_OK)
+async def generate_report_endpoint(report_request: ReportRequest):
+    pdf_name = report_request.pdf_name
     try:
-        # Verify the JWT token
-        user_email = verify_token(token)
-        if not user_email:
-            raise HTTPException(status_code=401, detail="Invalid or expired token")
-
-        # Extract summary parameters from the request body
-        document_name = summary_request.get("document_name")
-        response = summarize_document(document_name)
-        return {"markdown": response}
-
+        report_service = ReportService()
+        report_pdf_path = report_service.generate_report(pdf_name)
+        return FileResponse(report_pdf_path, media_type='application/pdf', filename='report.pdf')
     except Exception as e:
-        logging.error(f"An error occurred during the summary process: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"An error occurred while summarizing the document: {str(e)}")
-
+        raise HTTPException(status_code=500, detail=str(e))
 setup_chat_histories()
 
 
